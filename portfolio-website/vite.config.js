a1a6cfc2e68a486a92e8a088ec6fe1bbf8fb7f06
ABHISHEK SOUNDALGEKAR <abhimsound@gmail.com>
2025-08-07 06:11:03 -0700

    commmit

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/portfolio-website/',   // if deploying to GH Pages under /portfolio-website/
+  plugins: [ react() ],
+});
