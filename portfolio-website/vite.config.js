16f1f645c699de77b537cf5c011b550a413f145d
ABHISHEK SOUNDALGEKAR <abhimsound@gmail.com>
2025-08-07 06:24:09 -0700

    add

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio/',     // ← must exactly match your repo name
   plugins: [react()]
-});
+})
