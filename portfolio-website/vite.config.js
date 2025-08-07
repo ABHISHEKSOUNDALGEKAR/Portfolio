f0cdbd59203311c98746899b537320bbe3922592
ABHISHEK SOUNDALGEKAR <abhimsound@gmail.com>
2025-08-07 06:19:56 -0700

    add

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-website/',   // if deploying to GH Pages under /portfolio-website/
-  plugins: [ react() ],
+  base: '/Portfolio/',
+  plugins: [react()]
 });
