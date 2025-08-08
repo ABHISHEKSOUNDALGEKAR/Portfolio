302f7914e6ce24d1731e038f82a8f1acdd3e4846
ABHISHEK SOUNDALGEKAR <abhimsound@gmail.com>
2025-08-08 03:19:28 -0700

    adding change

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -5,7 +5,6 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Publication from './components/Publication';
 import About from './components/About';
-import Skills from './components/Skills';
 import Contact from './components/Contact';
 
 function App() {
